dab2657d801c98ab9cd8a16408aa8af2ec81dcf0
abhishekkumar <kr.skabhi@gmail.com>
2026-09-04 01:26:46 +0530

    Implemented Postgress Database .

---
+++ src/main/java/com/AI/AILEAD/configuration/ChatClientConfig.java
@@ -0,0 +1,23 @@
+//package com.AI.AILEAD.configuration;
+//
+//import org.springframework.ai.chat.client.ChatClient;
+//import org.springframework.ai.chat.memory.ChatMemory;
+//import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class ChatClientConfig {
+//
+//    @Bean
+//    public ChatClient chatClient(
+//            ChatClient.Builder builder,
+//            ChatMemory chatMemory) {
+//
+//        return builder
+//                .defaultAdvisors(
+//                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+//                )
+//                .build();
+//    }
+//}
--- src/main/java/com/AI/AILEAD/configuration/ChatMemoryConfig.java
+++ src/main/java/com/AI/AILEAD/configuration/ChatMemoryConfig.java
@@ -1,7 +1,10 @@
 package com.AI.AILEAD.configuration;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +12,24 @@ import org.springframework.context.annotation.Configuration;
 public class ChatMemoryConfig {
 
     @Bean
-    public ChatMemory chatMemory() {
+    public ChatMemory chatMemory(
+            JdbcChatMemoryRepository chatMemoryRepository) {
+
         return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository)
                 .maxMessages(20)
+                .build();
+    }
+
+    @Bean
+    public ChatClient chatClient(
+            ChatClient.Builder builder,
+            ChatMemory chatMemory) {
+
+        return builder
+                .defaultAdvisors(
+                        MessageChatMemoryAdvisor.builder(chatMemory).build()
+                )
                 .build();
     }
 }
